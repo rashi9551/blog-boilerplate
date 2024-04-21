06151e7d8dfcb3a4f59b85aa239fd3108084230a
rashi9551 <rashidroz1361@gmail.com>
2024-04-21 14:47:10 +0530

    comment andd post set

--- comments/index.js
+++ comments/index.js
@@ -1,17 +1,19 @@
 const express=require('express')
 const bodyParser=require('body-parser')
 const {randomBytes}=require('crypto')
+const cors=require('cors')
 
 const app=express()
 
 app.use(bodyParser.json())
+app.use(cors())
 
 const commentsByPostId={}
 
-app.get('/post/:id/comments',(req,res)=>{
+app.get('/posts/:id/comments',(req,res)=>{
     res.send(commentsByPostId[req.params.id]||[])
 })
-app.post('/post/:id/comments',(req,res)=>{
+app.post('/posts/:id/comments',(req,res)=>{
     const commentId=randomBytes(4).toString('hex')
     const {content}=req.body
     const comments=commentsByPostId[req.params.id]||[]
